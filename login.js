c4a1b7a1afba8a14bdf042c4f639049065beae3c
redjajaggg <102412196+redjajaggg@users.noreply.github.com>
2023-01-07 15:06:26 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,26 @@
+const password = window.prompt("Please Enter Password to Access the Website");
+
+if(password == "ok"){
+  alert("Successfully! Thank you!");
+  console.log(password);
+}
+else{
+  window.location.href = "https://a21acjuz391nyjljvyif1q.on.drv.tw/www.myprivatelove.com/page4.html";
+}
+
+function sh(){
+    var x = document.getElementById("dmp22center");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+}
+function hs(){
+    var x = document.getElementById("dmp23center");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+}
