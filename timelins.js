23c78536dff0a20718c2c2d9231c493c88eddd7e
redjajaggg <102412196+redjajaggg@users.noreply.github.com>
2023-02-17 18:57:23 +0700

    Add files via upload

--- timelins.js
+++ timelins.js
@@ -1,18 +1,21 @@
-const dataget = "https://docs.google.com/spreadsheets/d/1WT2dIeF9x0P1XBLQAm1C3PNMJum2NwYYZofGMeoPQW4/edit?usp=sharing";
-const datause = "https://docs.google.com/spreadsheets/d/1WT2dIeF9x0P1XBLQAm1C3PNMJum2NwYYZofGMeoPQW4/export?format=csv";
-const daa = "https://docs.google.com/spreadsheets/d/1RRUqHcL9Yq68XQA_XKyEFvyqpBsHD12x4OeP9QtOraY/export?format=csv";
+const datause = "https://docs.google.com/spreadsheets/d/1HsH40yVrlcWrSLXn0qEG06BR7X_YS4YUQyFbEzaEf7Y/export?format=csv";
 fetch(datause).then(result=>result.text()).then(function (csvtext){return csv().fromString(csvtext);}).then(function(csv){appenddata(csv);});
 
 function appenddata(data){
     var name = [];
+    var link = [];
+    var namepop = [];
+
     var show = document.getElementById("show");
-    var linkeach = [];
     
     for(var i = 0; i < data.length; i++){
-        name.push(data[i].NAME);
+        name.push(data[i].Name);
+        link.push(data[i].PictureLink);
+
+        create(data[i].Name);
     }
     var datalist = document.getElementById("names");
-    for(const names of name){
+    for(const names of namepop){
         const option = document.createElement('option');
         option.value = names;
         datalist.appendChild(option);
@@ -22,30 +25,57 @@ function appenddata(data){
         document.getElementById("show").innerHTML = "";
         name = [];
         link = [];
-        
-        fetch(daa).then(results=>results.text()).then(function (csvtexts){return csv().fromString(csvtexts);}).then(function(csvs){
-            var div = document.createElement("div");
-            var divbig = document.createElement("div");
-            var br = document.createElement("br");
-            for(var i = 0; i < csvs.length; i++){
-                //linkeach.push(csvs[i]);
-            }
-                
-        });
+        namepop = [];
         for(var i = 0; i < data.length; i++){
             name.push(data[i].Name);
+            link.push(data[i].PictureLink);
 
             create(namegets);
         }
     }
     document.getElementById("clearname").onclick = () => {document.getElementById("name").value = "";}
-    document.getElementById("show").innerHTML = "Choose one and Search...";
+    document.getElementById("show").innerHTML = "Click all or Choose one...";
     function create(name){
-            
+        
         var div = document.createElement("div");
         var divbig = document.createElement("div");
         var br = document.createElement("br");
-           
-    }
-}
 
+        if(!namepop.includes(name)){
+            namepop.push(name);
+            div.innerHTML = name;
+            div.style.backgroundColor = "#e6f542";
+            div.style.border = "3px solid black"
+            div.style.textAlign = "center";
+            div.style.marginLeft = "auto";
+            div.style.marginRight = "auto";
+            div.style.borderRadius = "5px";
+            div.style.fontFamily = "Brush Script MT";
+            div.style.fontSize = "18px";
+            divbig.appendChild(div);
+            divbig.style.backgroundColor = "#e3e8ac";
+            
+            divbig.style.borderRadius = "10px";
+            divbig.style.padding = "20px";
+            divbig.style.marginLeft = "auto";
+            divbig.style.marginRight = "auto";
+            divbig.style.fontSize = "0";
+
+            
+            for(var j = 0; j < data.length; j++){
+                if(data[j].Name == name){
+                    
+                    var imgs = document.createElement("img");
+                    imgs.setAttribute('src', data[j].PictureLink);
+                    if(window.innerWidth > 700){imgs.setAttribute('width', 400); divbig.style.width = "700px";}
+                    if(window.innerWidth <= 700){imgs.setAttribute('width', 280); divbig.style.width = "300px";}
+                    
+                    divbig.appendChild(imgs);
+                }
+            }
+            show.appendChild(br);
+            show.appendChild(divbig);
+        }
+    }
+
+}
