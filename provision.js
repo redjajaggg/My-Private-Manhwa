47598e666c10bc817c5a2a8f31a50d0c00f209c6
redjajaggg <102412196+redjajaggg@users.noreply.github.com>
2023-05-14 12:19:12 +0700

    Update provision.js

--- provision.js
+++ provision.js
@@ -24,7 +24,14 @@ function appenddata(data){
         if(data[i].Service == "Manhtia (ManhwaApp)"){
             div1.innerHTML = "No." + data[i].No + " : " + data[i].Method;
             div1.style.fontSize = "18px";
-            manhtia.appendChild(div1);
+            div2.innerHTML = ">>> " + data[i].Script;
+            img.setAttribute('src', data[i].Picture);
+            div3.innerHTML = "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^";
+            div1.style.fontSize = "18px";
+            website.appendChild(div1);
+            website.appendChild(div2);
+            if(data[i].Picture != "n"){website.appendChild(img);}
+            website.appendChild(div3);
         }
 
     }
